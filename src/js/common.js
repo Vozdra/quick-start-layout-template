0b4619e0660d530a17eafb08a9fb9ee04b73b4d5
Daria <vozdr@ukr.net>
2019-03-28 13:41:11 +0200

    modified and expanded

--- src/js/common.js
+++ src/js/common.js
@@ -1,1 +1,22 @@
-console.log('Included js')
+$(function() {
+
+  $(window).scroll(function() {
+		if ($(this).scrollTop() > $(this).height()) {
+			$('#to-top').addClass('show');
+		} else {
+			$('#to-top').removeClass('show');
+		}
+	});
+	
+	$('#to-top').click(function() {
+		$('html, body').stop().animate({scrollTop: 0}, 'slow', 'swing');
+  })
+
+  $("a[rel='scrollToId']").click(function() {
+    var targetDiv = $(this).attr('href');
+    $('html, body').animate({
+        scrollTop: $(targetDiv).offset().top
+    }, 1000);
+  });
+
+})
